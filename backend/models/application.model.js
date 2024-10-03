87291cfc86c830134a6727b8d4761646187e47c0
rohitgalande1032 <rohitg3234@gmail.com>
2024-10-03 11:00:21 +0530

    Create models

---
+++ backend/models/application.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { Job } from "./job.model";
+
+const applicationSchema = new mongoose.Schema({
+    Job: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Job",
+        required: true
+    },
+    applicant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'rejected'],
+        default: 'pending'
+    }
+},{timeseries: true})
+
+export const Application = mongoose.model("Application", applicationSchema)
