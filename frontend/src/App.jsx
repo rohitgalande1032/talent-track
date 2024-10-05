f4bb683fcab870f5bec3bf8d7d4f1223746f9f48
rohitgalande1032 <rohitg3234@gmail.com>
2024-10-05 11:23:39 +0530

    create navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter } from 'react-router-dom'
+import Navbar from './components/shared/Navbar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className='text-6xl'>
-        Hello
+        <Navbar />
       </div>
     </>
   )
